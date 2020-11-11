86f5372c32e0e5e7fd931996214d1e7e09c3728a
Tuerk, Halil Ibrahim (SE-A1/33) <halil.ibrahim.tuerk@volkswagen.de>
2020-11-11 23:03:46 +0100

    Updates

---
+++ precache-manifest.ceacfb39fa03fe19a574f57e66d07b20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3cc9f4c29ff2fa48c5a2d08c7450f93",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1e6f8c74adf127e3c87b",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "5acc662ceecf92f638fa",
+    "url": "./static/js/2.560d0b38.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.560d0b38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6f8c74adf127e3c87b",
+    "url": "./static/js/main.a47ebde4.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.a47ebde4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1682146309b162eb69",
+    "url": "./static/js/runtime-main.2d0a347d.js"
+  }
+]);
